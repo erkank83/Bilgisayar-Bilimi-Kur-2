969d1a515572fda5f59789965388cba215b8f8e6
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-10-02 16:10:49 +0300

    1- print() komutu

---
+++ ders1-print.py
@@ -0,0 +1,12 @@
+#print():Verilen parametreyi ekranda göstermek için kullanılır.
+print("Hoşgeldiniz")
+#Hoşgeldiniz
+print('İnsanların en hayırlısı, insanlara en çok faydalı olandır')
+#İnsanların en hayırlısı, insanlara en çok faydalı olandır
+print(5) #parametre sayısal bir ifade
+#5
+print(True)#parametre mantıksal bir ifade
+#True
+x=5
+print(x)#parametre değişken ismi
+#5
