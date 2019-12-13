07535a7772d5144d3ac6c1118eea5c46307b794d
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-13 12:14:25 +0300

    iç içe if örneği
    Karşılaştırma yapıları kullanırken bazı durumlarda istenilen koşulların birden fazla şarta aynı anda
    uyması istenebilir. Bu durumda koşul yapılarının birbirinin içinde kullanılması gerekir. Bu şekilde bir
    yapı kullanıldığında istenilen komut veya komut kümelerinin yapılması için iki koşul ifadesinin de True
    olması gerekir.

---
+++ Iciceif.py
@@ -0,0 +1,10 @@
+deger=int(input("Lütfen 0….10 aralığında bir tam sayı giriniz:"))
+if deger>=0 and deger<=10:
+    print("1)aralıkta")
+print("1)tamamlandı")
+
+if deger >=0: #ilk kontrol
+    if deger<=10:#ikinci kontrol
+        print("2)aralıkta")
+print("2)tamamlandı")
+    
