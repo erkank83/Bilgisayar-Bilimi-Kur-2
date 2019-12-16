bfb33bdde0cdfbf3cddf50420ab2f59b6daf4e7b
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-16 10:52:51 +0300

    else if
    Python’da çok yönlü koşullu durumlar iç içe if ifadeleri yanında if/elif/else ifadesi ile birlikte de kullanılabilir.
    Elif ifadesi, else if ifadesinin kısaltmasıdır. Örneğin,

---
+++ elseif.py
@@ -0,0 +1,23 @@
+# Klavyeden girilen 0-5 arasındaki sayıların yazı karşılığını veren program
+deger=int(input("Lütfen 0- 5 arasında bir değer girin: "))
+if deger < 0:
+    print("Çok küçük")
+elif deger == 0:
+    print("Sıfır")
+elif deger == 1:
+    print("Bir")
+elif deger == 2:
+    print("İki")
+elif deger == 2:
+    print("Bir")
+elif deger == 3:
+    print("Üç")
+elif deger == 4:
+    print("Dört")
+elif deger == 5:
+    print("Beş")
+else:
+    print("Çok büyük")
+
+print("Tamamlandı..")
+    
