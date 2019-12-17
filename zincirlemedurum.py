a113e8091d5fcf10661f3de2c7d3ed3dc756ac6d
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-17 12:30:06 +0300

    Zincirleme Durum İfadesi
    İkiden fazla olasılığın olduğu durumlarda ikiden fazla dallanmaya (yol) gereksinim duyarız. Bu tür durumlarda zincirleme koşul ifadeleri kullanılır. Her koşul sırasıyla sınanır. İlki yanlış ise sonraki kontrol edilir ve yazılan kodun tamamı bu şekilde çalıştırılır. Koşullardan biri doğru ise ilgili dal yürütülür ve cümlenin işlevi biter.

---
+++ zincirlemedurum.py
@@ -0,0 +1,12 @@
+value = int (input("Lütfen 0...3 arasında bir tam sayı giriniz:"))
+if value==0:
+    print("sıfır")
+if value==1: #Kontrol 2
+    print("bir") #Sonuç
+if value==2: #Kontrol 3
+    print("iki")
+if value==3: #Kontrol 4
+    print("üç")
+if value>3: #Kontrol 5
+    print("çok büyük")
+print("Tamamlandı")
