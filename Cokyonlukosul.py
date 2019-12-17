b2b3542f373d99453f21910dc883192d97272b5f
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-17 12:24:06 +0300

    Çok yönlü if ifadesi
    Yukarıdaki problemde kullanıcının klavyeden “1” girmesi durumunda işletilecek şartlar belirtilmiştir.
    Çok Yönlü Koşullu Durum yapısında Kontrol 1 kısmına girilecek fakat şart sağlanmadığı için
    sonuç kısmına girilmeyecek ve Kontrol 2 kısmına girilecektir. Geçilen bu aşamada şart sağlandığı için
    print() komutu işletilecek ve diğer tüm kontrol satırları atlanacaktır. Böylece fazla koşul kontrollerinden
    kaçınılmış olacaktır. Ancak Zincirleme Durum ifadesinde tüm şartlar sırasıyla kontrol edilecek, şartı
    sağlayanlar için print() satırları yapılacaktır

---
+++ Cokyonlukosul.py
@@ -0,0 +1,12 @@
+value = int (input("Lütfen 0...3 arasında bir tam sayı giriniz:"))
+if value==0: #Kontrol 1
+    print("sıfır")
+elif value==1: #Kontrol 2
+    print("bir")# Sonuç
+elif value==2:
+    print("iki")
+elif value==3:
+    print("üç")
+else:
+    print("çok büyük")
+print("Tamamlandı")
