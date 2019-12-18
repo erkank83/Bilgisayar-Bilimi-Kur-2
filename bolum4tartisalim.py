85be08914344c4d7be36c0b0dca029adb19cc42c
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-18 12:33:51 +0300

    tartısalim
    x, y, z = 3, 5, 7 olarak verilmektedir. Buna göre, aşağıda verilen işlemlerin sonucunu
    bulunuz.

---
+++ bolum4tartisalim.py
@@ -0,0 +1,15 @@
+x, y, z = 3, 5, 7
+print("x =",x,", y =",y,", z =",z)
+print("x == 3      ->",x == 3)
+print("x < y       ->",x < y)
+print("x >= y      ->",x >= y)
+print("x <= y      ->",x <= y)
+print("x != y - 2  ->",x != y - 2)
+print("x < 10      ->",x < 10)
+print("x >= 0  and x < 10",x >= 0 and x < 10)
+print("x < 0   and x < 10",x < 0 and x < 10)
+print("x >= 0  and x < 2",x >= 0 and x < 2)
+print("x < 0   or  x < 10",x < 0 or x < 10)
+print("x > 0   or  x < 10",x > 0 or x < 10)
+print("x < 0   or  x > 10",x < 0 or x > 10)
+
