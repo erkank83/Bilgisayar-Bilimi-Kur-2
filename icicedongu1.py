81d41eae4d2fc05a0972c13c14659013a3997258
erkank83 <55956112+erkank83@users.noreply.github.com>
2020-01-07 12:16:52 +0300

    tablo satır sütun örneği2

--- icicedongu1.py
+++ icicedongu1.py
@@ -1,4 +1,11 @@
-# Satır oluşturmak için
-sayi = int(input("Lütfen tablo ölçüsünü giriniz: "))
-for satir in range(1, sayi + 1):
-    print("Satır #", satir)
+# Hem satır hem de sütun oluşturmak için :
+satirsayisi = int(input("Tablonun satır sayısını giriniz: "))
+sutunsayisi = int(input("Tablonun sütun sayısını giriniz: "))
+for satir in range(1,satirsayisi+1):
+    for sutun in range(1,sutunsayisi+1):
+        print("[{0:2},{1:2}]".format(satir,sutun),sep="",end=" ")
+    print(end="\n")
+
+
+
+
